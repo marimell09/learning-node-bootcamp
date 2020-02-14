b6634a8f4715f9d7feb9371649f0d24df19f0574
marimell09 <mari_carvalhomello@hotmail.com>
2020-02-14 17:20:04 -0300

    Starting setcion 12, static site rendering.

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
@@ -13,6 +14,13 @@ const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+//Serving static files
+//app.use(express.static(`${__dirname}/public`));
+app.set(express.static(path.join(__dirname, 'public')));
 
 // 1) GLOBAL MIDDLEWARES
 
@@ -59,9 +67,6 @@ app.use(
   })
 );
 
-//Serving static files
-app.use(express.static(`${__dirname}/public`));
-
 // app.use((req, res, next) => {
 //   console.log('Hello from the middleware');
 //   //don't always forget to put next, because if you don't, the request response
@@ -82,6 +87,13 @@ app.use((req, res, next) => {
 //app.get('/api/v1/tours/:id', getTour);
 //app.patch('/api/v1/tours/:id', updateTour);
 //app.delete('/api/v1/tours/:id', deleteTour);
+
+app.get('/', (req, res) => {
+  res.status(200).render('base', {
+    tour: 'The Forest Hiker',
+    user: 'Mariana'
+  });
+});
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
